fb94f522a2d19dbb2933eec13bd20dcfba7e438e
SpeedRacer <robertgeorge2@gmail.com>
2021-06-04 11:52:37 -0400

    Renamed funciton generateLicenseButtonLink to generateMarkDown. AFixed undefined by adding return befo

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,28 +1,15 @@
-
-
-function renderLicenseBadgeLink(license) {
-  // let licenseBadge = 
-
+function generateMarkDown(license) {
   if (license === "MIT") {
-    `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
   }
   if (license === "GPL v2") {
-    `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
+    return `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
   }
   if (license === "Apache 2.0") {
-    `[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)`
+    return `[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)`
   }
   else {
-    `License: none`
-  };
-  
+    return `License: none`
+  }
 };
-
-// TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//     return `# ${data.title}
-  
-  // `;
-  // }
-  
-  module.exports = renderLicenseBadgeLink;
+  module.exports = generateMarkDown;
